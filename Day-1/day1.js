9b5d9bf444a0a8522a28a4496f43090bf93c7109
SatishK2022 <sk7399052@gmail.com>
2024-07-13 20:56:34 +0530

    day-1

---
+++ Day-1/day1.js
@@ -0,0 +1,47 @@
+// Activity 1 : Variable Declaration
+// Task 1
+var num = 10;
+console.log(num)
+
+// Task 2
+let name = "Satish";
+console.log(name)
+
+
+// Activity 2 : Constant Decleration
+// Task 3
+const isLoggedIn = false;
+console.log(isLoggedIn)
+
+
+// Activity 3 : Data Types
+// Task 4
+const num1 = 20;
+const var1 = "Hi there, How are you?"
+const isAdmin = true;
+const obj = {
+    firstName: "Satish",
+    lastName: "Kumar",
+    age: 20
+}
+const arr = [1, 2, 3, 4, 5]
+
+console.log(typeof num1)
+console.log(typeof var1)
+console.log(typeof isAdmin)
+console.log(typeof obj)
+console.log(typeof arr)
+
+
+// Activity 4 : Reassigning Variables
+// Task 5
+let marks = 50;
+console.log(marks)
+marks = 20
+console.log(marks)
+
+
+// Activity 5 : Understanding const
+// Task 6
+const score = 50
+// score = 30  // TypeError: Assignment to constant variable.
