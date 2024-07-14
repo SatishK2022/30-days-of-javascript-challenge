aafac2416cb9ef38b90d370f6f7a43c9d72b5505
SatishK2022 <sk7399052@gmail.com>
2024-07-14 13:08:57 +0530

    day-2

---
+++ Day-2/day2.js
@@ -0,0 +1,42 @@
+// Activity 1 : Arithmetic Operators
+let num1 = 10;
+let num2 = 5;
+
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num1 - num2);
+console.log("Product:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Remainder:", num1 % num2);
+
+// Activity 2 : Assignment Operators
+let x = 10;
+x += 5;
+console.log("The value of x is:", x);
+
+let y = 20;
+y -= 10;
+console.log("The value of y is:", y);
+
+// Activity 3 : Comparison Operators
+let a = 10;
+let b = 20;
+
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+
+// Activity 4 : Logical Operators
+let c = 10;
+let d = 20;
+
+console.log("c > 0 && d > 0:", c > 0 && d > 0);
+console.log("c > 0 || d > 0:", c > 0 || d > 0);
+console.log("!(c > 0):", !(c > 0));
+
+// Activity 5 : Ternary Operator
+let num = 10;
+let result = num > 0 ? "Positive" : "Negative";
+console.log(result);
